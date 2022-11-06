73814b91cf3a5847cc0e1ab79cd8ab25c7a5e3de
abhiyadav2345 <abhiyadav2345@gmail.com>
2022-11-06 15:31:41 +0530

    Merge Sort Added

--- sorting.cpp
+++ sorting.cpp
@@ -82,6 +82,56 @@ void selectionSort(int *array, int size) {
 
 //---------------------------------------------------Selection Sort End-------------------------------------------------
 
+//---------------------------------------------------Merge Sort-------------------------------------------------
+/* Algo
+
+*/
+void merge(int *arr, int p, int q, int r) {
+  int n1 = q - p + 1;
+  int n2 = r - q;
+ int L[n1], M[n2];
+ for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+ int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+ while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+ while (i < n1) {
+    arr[k] = L[i];
+    i++;
+k++;
+  }
+ while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int *arr, int left, int right) {
+  if (left < right) {
+    int mid = left + (right - left) / 2;
+ mergeSort(arr, left, mid);
+ mergeSort(arr, mid + 1, right);
+ merge(arr, left, mid, right);
+  }
+}
+
+
+//---------------------------------------------------Merge Sort End-------------------------------------------------
+
 int main(){
 
 }
