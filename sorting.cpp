2225132533c0b16789c19424b1f2d5d1aa0cc03d
abhiyadav2345 <abhiyadav2345@gmail.com>
2022-11-06 15:38:36 +0530

    case 5 bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -24,19 +24,20 @@ void averageCase(int *arr, int n)
     for (int i = 0; i < n; i++)
     {
         int m = rand() % n;
-        
+
         arr[i] = m;
     }
 }
 
-void reverseSortedArray(int n, int *arr){
-    for(int i = n; i>0; i--){
-        arr[n-i] = i;
-    } }
-
+void reverseSortedArray(int n, int *arr)
+{
+    for (int i = n; i > 0; i--)
+    {
+        arr[n - i] = i;
+    }
+}
 
 //----------------------------------------------------Array creation end---------------------------------------------
-
 
 //---------------------------------------------------Insertion Sort-------------------------------------------------
 
@@ -64,20 +65,21 @@ void insertsort(int *arr, int len)
 
 //---------------------------------------------------Insertion Sort End-------------------------------------------------
 
-
 //---------------------------------------------------Selection Sort-------------------------------------------------
 /* Algo
 
 */
-void selectionSort(int *array, int size) {
-   int i, j, imin;
-   for(i = 0; i<size-1; i++) {
-      imin = i;   
-      for(j = i+1; j<size; j++)
-         if(array[j] < array[imin])
-            imin = j;
-    swap(array[i], array[imin]);
-   }
+void selectionSort(int *array, int size)
+{
+    int i, j, imin;
+    for (i = 0; i < size - 1; i++)
+    {
+        imin = i;
+        for (j = i + 1; j < size; j++)
+            if (array[j] < array[imin])
+                imin = j;
+        swap(array[i], array[imin]);
+    }
 }
 
 //---------------------------------------------------Selection Sort End-------------------------------------------------
@@ -86,49 +88,57 @@ void selectionSort(int *array, int size) {
 /* Algo
 
 */
-void merge(int *arr, int p, int q, int r) {
-  int n1 = q - p + 1;
-  int n2 = r - q;
- int L[n1], M[n2];
- for (int i = 0; i < n1; i++)
-    L[i] = arr[p + i];
-  for (int j = 0; j < n2; j++)
-    M[j] = arr[q + 1 + j];
- int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
- while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = M[j];
-      j++;
+void merge(int *arr, int p, int q, int r)
+{
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int L[n1], M[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
     }
-    k++;
-  }
- while (i < n1) {
-    arr[k] = L[i];
-    i++;
-k++;
-  }
- while (j < n2) {
-    arr[k] = M[j];
-    j++;
-    k++;
-  }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
 }
 
-void mergeSort(int *arr, int left, int right) {
-  if (left < right) {
-    int mid = left + (right - left) / 2;
- mergeSort(arr, left, mid);
- mergeSort(arr, mid + 1, right);
- merge(arr, left, mid, right);
-  }
+void mergeSort(int *arr, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
 }
-
 
 //---------------------------------------------------Merge Sort End-------------------------------------------------
 
@@ -145,42 +155,43 @@ int getMax(int arr[], int n)
             mx = arr[i];
     return mx;
 }
- 
+
 // A function to do counting sort of arr[] according to
 // the digit represented by exp.
 void countSort(int arr[], int n, int exp)
 {
     int output[n]; // output array
-    int i, count[10] = { 0 };
- 
+    int i, count[10] = {0};
+
     // Store count of occurrences in count[]
     for (i = 0; i < n; i++)
         count[(arr[i] / exp) % 10]++;
- 
+
     // Change count[i] so that count[i] now contains actual
     //  position of this digit in output[]
     for (i = 1; i < 10; i++)
         count[i] += count[i - 1];
- 
+
     // Build the output array
-    for (i = n - 1; i >= 0; i--) {
+    for (i = n - 1; i >= 0; i--)
+    {
         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
- 
+
     // Copy the output array to arr[], so that arr[] now
     // contains sorted numbers according to current digit
     for (i = 0; i < n; i++)
         arr[i] = output[i];
 }
- 
+
 // The main function to that sorts arr[] of size n using
 // Radix Sort
 void radixsort(int arr[], int n)
 {
     // Find the maximum number to know number of digits
     int m = getMax(arr, n);
- 
+
     // Do counting sort for every digit. Note that instead
     // of passing digit number, exp is passed. exp is 10^i
     // where i is current digit number
@@ -194,19 +205,21 @@ void radixsort(int arr[], int n)
 
 */
 
-void bubbleSort(int *arr,int n){
+void bubbleSort(int *arr, int n)
+{
     int i, j;
     for (i = 0; i < n - 1; i++)
- 
+
         // Last i elements are already in place
         for (j = 0; j < n - i - 1; j++)
             if (arr[j] > arr[j + 1])
-swap(arr[j], arr[j + 1]);
+                swap(arr[j], arr[j + 1]);
 }
 
 //---------------------------------------------------Bubble Sort End-------------------------------------------------
 
-int main(){
+int main()
+{
 
     clock_t t;
 
@@ -325,7 +338,7 @@ int main(){
         cout << "time taken for Worst case in insertion sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         break;
-//--------------------------------------------------insertion Sort End-------------------------------------------------
+        //--------------------------------------------------insertion Sort End-------------------------------------------------
         //---------------------------------------------------Selection Sort-------------------------------------------------
     case 2:
         //------------------------------------Size of Array 50000-------------------------------------------
@@ -440,21 +453,21 @@ int main(){
         /* Best Case*/
         bestCase(arr, 50000);
         t = clock();
-        mergeSort(arr, 0,50000);
+        mergeSort(arr, 0, 50000);
         t = clock() - t;
         cout << "time taken for best case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Average*/
         averageCase(arr, 50000);
         t = clock();
-        mergeSort(arr,0, 50000);
+        mergeSort(arr, 0, 50000);
         t = clock() - t;
         cout << "time taken for Average case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Worst */
         worstCase(arr, 50000);
         t = clock();
-        mergeSort(arr,0,50000);
+        mergeSort(arr, 0, 50000);
         t = clock() - t;
         cout << "time taken for Worst case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
@@ -465,21 +478,21 @@ int main(){
         /* Best Case*/
         bestCase(arr, 25000);
         t = clock();
-        mergeSort(arr, 0,25000);
+        mergeSort(arr, 0, 25000);
         t = clock() - t;
         cout << "time taken for best case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Average*/
         averageCase(arr, 25000);
         t = clock();
-        mergeSort(arr,0, 25000);
+        mergeSort(arr, 0, 25000);
         t = clock() - t;
         cout << "time taken for Average case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Worst */
         worstCase(arr, 25000);
         t = clock();
-        mergeSort(arr,0, 25000);
+        mergeSort(arr, 0, 25000);
         t = clock() - t;
         cout << "time taken for Worst case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
@@ -490,21 +503,21 @@ int main(){
         /* Best Case*/
         bestCase(arr, 10000);
         t = clock();
-        mergeSort(arr, 0,10000);
+        mergeSort(arr, 0, 10000);
         t = clock() - t;
         cout << "time taken for best case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Average*/
         averageCase(arr, 10000);
         t = clock();
-        mergeSort(arr,0, 10000);
+        mergeSort(arr, 0, 10000);
         t = clock() - t;
         cout << "time taken for Average case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Worst */
         worstCase(arr, 10000);
         t = clock();
-        mergeSort(arr,0, 10000);
+        mergeSort(arr, 0, 10000);
         t = clock() - t;
         cout << "time taken for Worst case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
@@ -515,21 +528,21 @@ int main(){
         /* Best Case*/
         bestCase(arr, 5000);
         t = clock();
-        mergeSort(arr,0, 5000);
+        mergeSort(arr, 0, 5000);
         t = clock() - t;
         cout << "time taken for best case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Average*/
         averageCase(arr, 5000);
         t = clock();
-        mergeSort(arr,0, 5000);
+        mergeSort(arr, 0, 5000);
         t = clock() - t;
         cout << "time taken for Average case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
         /* Worst */
-        worstCase(arr,5000);
+        worstCase(arr, 5000);
         t = clock();
-        mergeSort(arr,0, 5000);
+        mergeSort(arr, 0, 5000);
         t = clock() - t;
         cout << "time taken for Worst case in Merge sort: " << (float)t / CLOCKS_PER_SEC << endl;
 
@@ -643,5 +656,112 @@ int main(){
 
         //---------------------------------------------------Radix Sort End-------------------------------------------------
 
+        //---------------------------------------------------Bubble Sort-------------------------------------------------
+    case 5:
+        //------------------------------------Size of Array 50000-------------------------------------------
+        cout << "Size of Array : 50000" << endl;
+        cout << "Best, Average and Worst Case for Bubble sort" << endl;
 
+        /* Best Case*/
+        bestCase(arr, 50000);
+        t = clock();
+        bubbleSort(arr, 50000);
+        t = clock() - t;
+        cout << "time taken for best case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Average*/
+        averageCase(arr, 50000);
+        t = clock();
+        bubbleSort(arr, 50000);
+        t = clock() - t;
+        cout << "time taken for Average case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Worst */
+        worstCase(arr, 50000);
+        t = clock();
+        bubbleSort(arr, 50000);
+        t = clock() - t;
+        cout << "time taken for Worst case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        //------------------------------------------------------- Size of Array 25000-------------------------------
+        cout << "\nSize of Array : 25000" << endl;
+        cout << "Best, Average and Worst Case for Bubble sort" << endl;
+
+        /* Best Case*/
+        bestCase(arr, 25000);
+        t = clock();
+        bubbleSort(arr, 25000);
+        t = clock() - t;
+        cout << "time taken for best case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Average*/
+        averageCase(arr, 25000);
+        t = clock();
+        bubbleSort(arr, 25000);
+        t = clock() - t;
+        cout << "time taken for Average case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Worst */
+        worstCase(arr, 25000);
+        t = clock();
+        bubbleSort(arr, 25000);
+        t = clock() - t;
+        cout << "time taken for Worst case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        //------------------------------------------------------- Size of Array 10000-------------------------------
+        cout << "\nSize of Array : 10000" << endl;
+        cout << "Best, Average and Worst Case for Bubble sort" << endl;
+
+        /* Best Case*/
+        bestCase(arr, 10000);
+        t = clock();
+        bubbleSort(arr, 10000);
+        t = clock() - t;
+        cout << "time taken for best case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Average*/
+        averageCase(arr, 10000);
+        t = clock();
+        bubbleSort(arr, 10000);
+        t = clock() - t;
+        cout << "time taken for Average case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Worst */
+        worstCase(arr, 10000);
+        t = clock();
+        bubbleSort(arr, 10000);
+        t = clock() - t;
+        cout << "time taken for Worst case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        //------------------------------------------------------- Size of Array 5000-------------------------------
+        cout << "\nSize of Array : 5000" << endl;
+        cout << "Best, Average and Worst Case for Bubble sort" << endl;
+
+        /* Best Case*/
+        bestCase(arr, 5000);
+        t = clock();
+        bubbleSort(arr, 5000);
+        t = clock() - t;
+        cout << "time taken for best case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Average*/
+        averageCase(arr, 5000);
+        t = clock();
+        bubbleSort(arr, 5000);
+        t = clock() - t;
+        cout << "time taken for Average case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        /* Worst */
+        worstCase(arr, 5000);
+        t = clock();
+        bubbleSort(arr, 5000);
+        t = clock() - t;
+        cout << "time taken for Worst case in Bubble sort: " << (float)t / CLOCKS_PER_SEC << endl;
+
+        break;
+        //---------------------------------------------------Bubble Sort End-------------------------------------------------
+    default:
+
+        break;
+    }
 }
